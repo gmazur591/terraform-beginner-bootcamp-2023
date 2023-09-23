7eddc904c76dfd36d54342aa4c8383a843d20fe5
gmazur591 <gmazur591@gmail.com>
2023-09-23 10:47:16 +0000

    WIP: create temp bucket

--- main.tf
+++ main.tf
@@ -1,1 +1,24 @@
+terraform {
+  cloud {
+    organization = "tf-bootcamp-21"
 
+    workspaces {
+      name = "terratowns"
+    }
+  }
+}
+
+data "aws_caller_identity" "current" {}
+
+resource "random_pet" "bucket_name" {
+  length    = 3
+  separator = "-"
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "tf-bucket-${random_pet.bucket_name.id}"
+}
+
+output "bucket_name" {
+  value = aws_s3_bucket.my_bucket.bucket
+}
---
+++ providers.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "3.5.1"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.17.0"
+    }
+  }
+}
+
+provider "aws" {
+}
